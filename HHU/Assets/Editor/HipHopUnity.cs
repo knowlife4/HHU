d3fbfe1cd41219dd440be27b01740d6170559249
Isaac Crawford <knowlife4@gmail.com>
2020-04-24 19:27:05 -0500

    Base import script

---
+++ HHU/Assets/Editor/HipHopUnity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class HipHopUnity : EditorWindow
+{
+    public Object HIP;
+    public Object HOP;
+
+    [MenuItem("Window/HHU Toolset")]
+
+    public static void ShowWindow ()
+    {
+        GetWindow<HipHopUnity>("HipHopUnity");
+    }
+
+    void OnGUI()
+    {
+       EditorGUILayout.ObjectField(HIP, typeof(Object), true);
+       EditorGUILayout.ObjectField(HOP, typeof(Object), true);
+    }
+}
---
+++ HHU/Assets/Scripts/HIPImporter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using System.IO;
+using HipHopFile;
+using RenderWareFile;
+
+
+[ScriptedImporter(1, "HIP")]
+public class HIPImporter: ScriptedImporter {
+
+ public override void OnImportAsset(AssetImportContext ctx) {
+  int pos = ctx.assetPath.LastIndexOf("/") + 1;
+  string LevelExtension = ctx.assetPath.Substring(pos, ctx.assetPath.Length - pos);
+  string LevelName = LevelExtension.Substring(0, LevelExtension.IndexOf("."));
+  var hipFile = new HipFile(ctx.assetPath);
+
+  if (!AssetDatabase.IsValidFolder("Assets/" + LevelName)) {
+   AssetDatabase.CreateFolder("Assets", LevelName);
+  foreach (var AHDR in hipFile.DICT.ATOC.AHDRList){
+  if (AHDR.assetType == AssetType.MODL)
+    {
+        var model = ReadFileMethods.ReadRenderWareFile(AHDR.data)[0];
+        
+
+    }
+  }
+  }
+
+ }
+}
